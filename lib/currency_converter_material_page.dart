458d9113b81d961543c1f84322cbc62718f02905
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-13 14:40:38 +0100

    first commit

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "0",
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
