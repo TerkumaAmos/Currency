2f2a34966aa6aa9c346aaca10fbbb89eaf498d91
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-14 01:37:54 +0100

    new

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -4,6 +4,7 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_declarations
     final border = const OutlineInputBorder(
       borderSide: BorderSide(
         //color: Colors.red,
@@ -30,23 +31,29 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
-              TextField(
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: InputDecoration(
-                  hintText: "Please enter the amount in USDT",
-                  hintStyle: const TextStyle(
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  style: const TextStyle(
                     color: Colors.black,
                   ),
-                  prefixIcon: const Icon(
-                    Icons.monetization_on_outlined,
+                  decoration: InputDecoration(
+                    hintText: "Please enter the amount in USDT",
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.monetization_on_outlined,
+                    ),
+                    prefixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: border,
+                    enabledBorder: border,
                   ),
-                  prefixIconColor: Colors.black,
-                  filled: true,
-                  fillColor: Colors.white,
-                  focusedBorder: border,
-                  enabledBorder: border,
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
                 ),
               ),
             ],
